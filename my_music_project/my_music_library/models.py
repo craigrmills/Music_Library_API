1a79dddcabf998cb957eed9e96d897b2250acf74
craigrmills <craig.r.mills86@gmail.com>
2021-08-13 11:25:06 -0500

    Added Genre to Models

--- my_music_project/my_music_library/models.py
+++ my_music_project/my_music_library/models.py
@@ -5,6 +5,7 @@ class Song(models.Model):
     title = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
     album = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50, default="Uncatigorized")
     release_date = models.DateField()
 
     def __str__(self):
